dd3e1ce9a95c9a623c25cdf69c466eb1eb9dac3c
Aryan Kumar <aryanak9163@gmail.com>
2023-05-10 23:30:03 +0530

    understanding global css in nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
 import '@/styles/globals.css'
+import '../styles/style.css'
+
+// we can use global css by importing them here
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
